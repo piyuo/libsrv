cce791c937383b6fb818ed36ddb01dcc1f922ab1
piyuo <piyuo.com@gmail.com>
2020-01-31 12:37:51 -0800

    update global credential test

--- secure/gcp/gcp_test.go
+++ secure/gcp/gcp_test.go
@@ -10,14 +10,25 @@ import (
 func TestCredential(t *testing.T) {
 
 	Convey("should init google credential", t, func() {
-		cred, _ := createCredential(context.Background(), "log-gcp", "https://www.googleapis.com/auth/cloud-platform")
+		cred, err := createCredential(context.Background(), "log-gcp", "https://www.googleapis.com/auth/cloud-platform")
+		So(err, ShouldBeNil)
 		So(cred, ShouldNotBeNil)
 	})
 
-	Convey("should keep google credential", t, func() {
-		So(logCred, ShouldBeNil)
-		cred, _ := LogCredential(context.Background())
+	Convey("should keep global log google credential", t, func() {
+		So(globalCredentialLog, ShouldBeNil)
+		cred, err := LogCredential(context.Background())
+		So(err, ShouldBeNil)
 		So(cred, ShouldNotBeNil)
-		So(logCred, ShouldNotBeNil)
+		So(globalCredentialLog, ShouldNotBeNil)
 	})
+
+	Convey("should keep global data google credential", t, func() {
+		So(globalCredentialData, ShouldBeNil)
+		cred, err := DataCredential(context.Background())
+		So(err, ShouldBeNil)
+		So(cred, ShouldNotBeNil)
+		So(globalCredentialData, ShouldNotBeNil)
+	})
+
 }
