0e8877619e075e0c14fc145cc9c85af6bd7f1365
piyuo <piyuo.com@gmail.com>
2021-03-02 14:13:52 -0800

    add regions definition

--- src/gcloud/gcloud.go
+++ src/gcloud/gcloud.go
@@ -19,6 +19,14 @@ const defaultQueueID = "tasks"
 
 const defaultLocationID = "us-central1"
 
+// Regions is predefine google regions for deploy cloud run and database
+//
+var Regions = map[string]string{
+	"us": "us-central1",
+	"jp": "asia-northeast1",
+	"be": "europe-west1",
+}
+
 // testMode is true should return success, false return error, otherwise behave normal
 //
 var testMode *bool
