4b3f2b1805c0599ac327940c2fcd8ac0c046c5f1
piyuo <piyuo.com@gmail.com>
2021-03-01 17:29:25 -0800

    use predefine location,queue when create task

--- src/gcloud/gcloud.go
+++ src/gcloud/gcloud.go
@@ -6,23 +6,33 @@ import (
 	"time"
 
 	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
+	"github.com/piyuo/libsrv/src/gaccount"
 	"github.com/pkg/errors"
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	tasks "google.golang.org/genproto/googleapis/cloud/tasks/v2"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 const here = "gcloud"
 
-// CreateHTTPTask create http task on google cloud task
+const defaultQueueID = "tasks"
+
+const defaultLocationID = "us-central1"
+
+// CreateHTTPTask create google cloud task, if scheduleTime is nil mean now
 //
-//	err = gcloud.CreateHTTPTask(ctx, "my-project","us-central1","my-queue",url,body)
+//	err = gcloud.CreateHTTPTask(ctx, url,body,nil)
 //
-func CreateHTTPTask(ctx context.Context, cred *google.Credentials, projectID, locationID, queueID, url string, body []byte) error {
+func CreateHTTPTask(ctx context.Context, url string, body []byte, scheduleTime *timestamppb.Timestamp) error {
 
 	//gcloud won't allow context deadline over 30 seconds
 	ctx, cancel := context.WithTimeout(ctx, time.Second*20) // cloud flare dns call must completed in 15 seconds
 	defer cancel()
+
+	cred, err := gaccount.GlobalCredential(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to create global credential")
+	}
 
 	client, err := cloudtasks.NewClient(ctx, option.WithCredentials(cred))
 	if err != nil {
@@ -30,12 +40,13 @@ func CreateHTTPTask(ctx context.Context, cred *google.Credentials, projectID, lo
 	}
 
 	// Build the Task queue path.
-	queuePath := fmt.Sprintf("projects/%s/locations/%s/queues/%s", projectID, locationID, queueID)
+	queuePath := fmt.Sprintf("projects/%s/locations/%s/queues/%s", cred.ProjectID, defaultLocationID, defaultQueueID)
 
 	// Build the Task payload.
 	req := &tasks.CreateTaskRequest{
 		Parent: queuePath,
 		Task: &tasks.Task{
+			ScheduleTime: scheduleTime,
 			MessageType: &tasks.Task_HttpRequest{
 				HttpRequest: &tasks.HttpRequest{
 					HttpMethod: tasks.HttpMethod_POST,
--- src/gcloud/gcloud_test.go
+++ src/gcloud/gcloud_test.go
@@ -4,19 +4,13 @@ import (
 	"context"
 	"testing"
 
-	"github.com/piyuo/libsrv/src/gaccount"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGcloudCreateHTTPTask(t *testing.T) {
 	assert := assert.New(t)
 	ctx := context.Background()
-	cred, err := gaccount.GlobalCredential(ctx)
-	assert.Nil(err)
 
-	err = CreateHTTPTask(ctx, cred, "piyuo-beta", "us-central1", "ci-queue", "http://notExist", []byte{})
+	err := CreateHTTPTask(ctx, "http://notExist", []byte{}, nil)
 	assert.Nil(err)
-
-	err = CreateHTTPTask(ctx, cred, "notExist", "notExist", "notExist", "http://notExist", []byte{})
-	assert.Contains(err.Error(), "not exist")
 }
