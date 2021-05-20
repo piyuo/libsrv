976e462df53c6f3eea09c92c320db57ee83ccf8e
piyuo <piyuo.com@gmail.com>
2021-05-19 22:42:28 -0700

    add firewall const

---
+++ firewall/firewall.go
@@ -0,0 +1,15 @@
+package file
+
+const (
+	// BlockShort block short period of time, usually 5 minutes
+	BlockShort = "BLOCK_SHORT"
+
+	// BlockLong block long period of time, usually 1 hour
+	BlockLong = "BLOCK_LONG"
+
+	// BlockDay block 24 hour
+	BlockDay = "BLOCK_24"
+
+	// Block second call, this happen when same call happen twice in 10 seconds
+	BlockHighFrequency = "BLOCK_HIGH_FREQ"
+)
